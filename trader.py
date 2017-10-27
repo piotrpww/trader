545b0ce9095c04aa796cdb555c413c576bebf384
Piotr <piotranon@gmail.com>
2017-10-27 11:46:26 +0200

    1. I added new functoin that calculcates weighted average sum 2. All bitcoin trades are stored in sqlite DB 3. New function to impelement

--- trader.py
+++ trader.py
@@ -6,27 +6,59 @@ import sqlite3
 import time
 
 
-def get_bitcoin_trades(http_api, last_transaction):
-        r = requests.get(http_api)
-        curr = r.json()[1]
-        curr_date = r.json()[1]['date']
-        date_sum = 0
-        next_date = 0
-        for event in reversed(r.json()):
-            print event
-            next_event = event
-            date_sum += ( next_event['date'] - curr['date'] )
-            curr = event
-        print date_sum
+
+
+def get_bitcoin_trades():
+        db = sqlite3.connect("trader.sql3")
+        c = db.cursor()
+        c.execute("select max(tid) from btcpln_trades")
+        max_tid = int(c.fetchone()[0]) 
+        BITBAY_TRADES    =  "https://bitbay.net/API/Public/BTCPLN/trades.json?since=%s" % int(max_tid)
+        r = requests.get(BITBAY_TRADES)
+        tables = r.json()
+        columns = ['date', 'price', 'type', 'amount', 'tid']
+        for element in tables:
+            print element
+            for dtype, value in element.iteritems():
+               keys = tuple(element[field] for field in columns)
+            query = "insert into btcpln_trades values (?, ?, ?, ?, ?)"
+            c = db.cursor()
+            c.execute(query, keys)
+        c.close()
+        db.commit()
+
+
+
+def get_bitcoin_average_weight(period):
+      current_time = int(time.time())
+      fmin_before =  current_time - period*60*60
+      db = sqlite3.connect("trader.sql3")
+      c = db.cursor()
+
+      c.execute("select date, amount, price from btcpln_trades where date > %s" % fmin_before)
+      total_sum    = 0.0
+      total_weight = 0.0
+      for cos in c.fetchall():
+              total_sum += float(cos[1])*float(cos[2])
+              total_weight += float(cos[1])
+      average_weight = total_sum/total_weight
+      print average_weight
+    
+
+#def get_bitcoin_average(period)
+     
+
+
 
 while True:
           BITBAY_TRADES    =  "https://bitbay.net/API/Public/BTCPLN/trades.json?sort=desc"
           BITBAY_ORDERBOOK =  "https://bitbay.net/API/Public/BTCPLN/orderbook.json?sort=desc"
-          trades = get_bitcoin_trades(BITBAY_TRADES, '1895840')
-          time.sleep(3)
+          trades = get_bitcoin_trades()
+          get_bitcoin_average_weight(0.5)
+          get_bitcoin_average_weight(3)
+          get_bitcoin_average_weight(6)
           break
 #          trades_sum=0
 #          con = sqlite3.connect('btc_trading.slite')
 #          cur = con.cursor()
 #          cur.execute('SELECT SQLITE_VERSION()')
-
